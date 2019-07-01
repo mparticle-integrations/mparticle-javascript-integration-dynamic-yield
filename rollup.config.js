62ce742b2ee658e4ceb7ee6fc10a943d9a9819cc
Robert Ing <ring@mparticle.com>
2019-07-01 15:49:18 -0400

    Update rollup to build IIFE and CJS, refactor register

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,53 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-export default [{
-    input: 'src/DynamicYieldKit.js',
-    output: {
-        file: 'DynamicYieldKit.js',
-        format: 'umd',
-        exports: 'named',
-        name: 'mp-dynamicYield-kit',
-        strict: false
+export default [
+    {
+        input: 'src/DynamicYieldKit.js',
+        output: {
+            file: 'DynamicYieldKit.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpDynamicYieldKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
     },
-    plugins: [
-        resolve({
-            browser: true
-        }),
-        commonjs()
-    ]
-},
-{
-    input: 'src/DynamicYieldKit.js',
-    output: {
-        file: 'dist/DynamicYieldKit.js',
-        format: 'umd',
-        exports: 'named',
-        name: 'mp-dynamicYield-kit',
-        strict: false
+    {
+        input: 'src/DynamicYieldKit.js',
+        output: {
+            file: 'dist/DynamicYieldKit.iife.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpDynamicYieldKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
     },
-    plugins: [
-        resolve({
-            browser: true
-        }),
-        commonjs()
-    ]
-}
-] 
+    {
+        input: 'src/DynamicYieldKit.js',
+        output: {
+            file: 'dist/DynamicYieldKit.common.js',
+            format: 'cjs',
+            exports: 'named',
+            name: 'mpDynamicYieldKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
+    }
+]
